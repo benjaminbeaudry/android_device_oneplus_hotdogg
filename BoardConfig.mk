afdd3574e44161e08d1021d9c450a79351f9c39a
benjaminbeaudry <bobeaudry@gmail.com>
2024-11-16 14:43:42 -0800

    hotdogg: convert from hotdog to hotdogg

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,7 +7,7 @@
 # Include the common OEM chipset BoardConfig.
 include device/oneplus/sm8150-common/BoardConfigCommon.mk
 
-DEVICE_PATH := device/oneplus/hotdog
+DEVICE_PATH := device/oneplus/hotdogg
 
 # A/B
 AB_OTA_PARTITIONS += \
@@ -51,6 +51,9 @@ TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/init/fstab.qcom
 
+# SELinux
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
 # Sensors
 SOONG_CONFIG_ONEPLUS_MSMNILE_SENSORS_ALS_POS_X := 940
 SOONG_CONFIG_ONEPLUS_MSMNILE_SENSORS_ALS_POS_Y := 260
@@ -63,4 +66,4 @@ BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX_LOCATION := 1
 
 # Include the proprietary files BoardConfig.
-include vendor/oneplus/hotdog/BoardConfigVendor.mk
+include vendor/oneplus/hotdogg/BoardConfigVendor.mk
--- lineage_hotdogg.mk
+++ lineage_hotdogg.mk
@@ -8,27 +8,27 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hotdog device
-$(call inherit-product, device/oneplus/hotdog/device.mk)
+# Inherit from hotdogg device
+$(call inherit-product, device/oneplus/hotdogg/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_hotdog
-PRODUCT_DEVICE := hotdog
+PRODUCT_NAME := lineage_hotdogg
+PRODUCT_DEVICE := hotdogg
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_MODEL := HD1911
 PRODUCT_BRAND := OnePlus
 
-PRODUCT_SYSTEM_NAME := OnePlus7TPro
-PRODUCT_SYSTEM_DEVICE := OnePlus7TPro
+PRODUCT_SYSTEM_NAME := OnePlus7TProNR
+PRODUCT_SYSTEM_DEVICE := OnePlus7TProNR
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus7TPro-user 12 SKQ1.211113.001 Q.202212132005 release-keys" \
+    PRIVATE_BUILD_DESC="OnePlus7TProNR-user 11 RKQ1.201022.002 2108021431 release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
-BUILD_FINGERPRINT := OnePlus/OnePlus7TPro/OnePlus7TPro:12/SKQ1.211113.001/Q.202212132005:user/release-keys
+BUILD_FINGERPRINT := OnePlus/OnePlus7TProNR/OnePlus7TProNR:11/RKQ1.201022.002/2108021431:user/release-keys
